42f838daaecafe752e48599a5f27055e7016bf13
Hannah Highfill <hannahhighfill@gmail.com>
2019-02-27 20:02:22 -0800

    cleaned up

--- build.py
+++ build.py
@@ -17,14 +17,12 @@ pages = [
 ]
     
 def main():
-#    page_title = page ['title']
     for page in pages:
         top = open('templates/top.html').read()
         middle = open(page['filename']).read()      
         bottom = open('templates/bottom.html').read()
         combined = top + middle + bottom
-        open(page["output"],"w+").write(combined)
-# do the same thing here that I learned in middle    
+        open(page["output"],"w+").write(combined)    
     
 print("Built!")
     
@@ -33,26 +31,3 @@ if __name__== "__main__":
     main()
 
 print("again")
-
-
-
-#print('Building Home page')
-#top_html = open('templates/top.html').read()
-#middle_html = open('content/home.html').read()
-#bottom_html = open('templates/bottom.html').read()
-#combined_html = top_html + middle_html + bottom_html
-#open('docs/index.html',"w+").write(combined_html)
-
-#print('Building Blog page')
-#top_html = open('templates/top.html').read()
-#middle_html = open('content/blog.html').read()
-#bottom_html=open('templates/bottom.html').read()
-#combined_html = top_html +middle_html + bottom_html
-#open('docs/blog.html',"w+").write(combined_html)
-
-#print('Building projects page')
-#top_html = open('templates/top.html').read()
-#middle_html = open('content/projects.html').read()
-#bottom_html=open('templates/bottom.html').read()
-#combined_html = top_html +middle_html + bottom_html
-#open('docs/projects.html',"w+").write(combined_html)
